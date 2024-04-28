1452e1760707b6b4c1feba7ddb1cc0eb2c3502c6
Xiaohan Wang <xiaohan_wang@unc.edu>
2024-04-28 14:57:49 -0400

    dashboard progress.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -51,9 +51,13 @@ export default function Dashboard() {
       <div className="album py-5 bg-body-tertiary">
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {trips.map((trip, index) => (
-              <TripCard key={index} trip={trip} />
-            ))}
+            {trips.length > 0 ? (
+              trips.map((trip, index) => <TripCard key={index} trip={trip} />)
+            ) : (
+              <div className="text-center">
+                <h2>No upcoming trips for you...</h2>
+              </div>
+            )}
           </div>
         </div>
       </div>
