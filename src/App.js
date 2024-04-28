767d84ca7011baf6e996cd1b5102e62a7cb098f0
Xiaohan Wang <xiaohan_wang@unc.edu>
2024-04-28 11:31:50 -0400

    Home page carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import './App.css';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import LogIn from './components/LogIn';
@@ -10,22 +9,33 @@ import LogIn from './components/LogIn';
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header>
-        <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/dashboard">Dashboard</Link></li>
-              <li><Link to="/login">Log In</Link></li>
-            </ul>
-          </nav>
+      <div className="App">
+      <header data-bs-theme="dark">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Trippy</Link> 
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/dashboard">Dashboard</Link> 
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Log In</Link> 
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
-      <Routes>
-      <Route path="" element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="login" element={<LogIn />} />
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<LogIn />} />
         </Routes>
-    </div>
+      </div>
     </Router>
   );
 }
