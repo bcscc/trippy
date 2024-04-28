76c212fa9139bd39f9406dc56fc99ad79f5b9e8d
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2024-04-28 12:52:42 -0400

    Fix App.js Merge Error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-import Home from './components/Home';
-import Dashboard from './components/Dashboard';
-import LogIn from './components/LogIn';
-
-import "./App.css";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
 import LogIn from "./components/LogIn";
