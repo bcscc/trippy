4a065f5b1f8dec53b968ba198e4c87d05e57007e
Xiaohan Wang <xiaohan_wang@unc.edu>
2024-04-28 14:53:27 -0400

    dashboard progress

---
+++ src/components/tripcard.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function TripCard({ trip }) {
+  return (
+    <div className="col">
+      <div className="card shadow-sm">
+        <svg>
+          <image
+            xlinkHref="https://media.istockphoto.com/id/1355017918/photo/passenger-airplane-taking-of-at-sunrise.webp?b=1&s=170667a&w=0&k=20&c=PPbjTcZHqycV0G1gkG0GRpEnmf1BOO0Cf07ltf1_tX4="
+            width="100%"
+          />
+        </svg>
+        <div className="card-body">
+          <p className="card-text">{trip.destination} - {trip.description}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-body-secondary">{trip.date}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
