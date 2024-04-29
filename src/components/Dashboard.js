02184a2ea231034a29af0af5393b1e8954cb3b73
Xiaohan Wang <xiaohan_wang@unc.edu>
2024-04-28 20:23:07 -0400

    dashboard fix

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -51,7 +51,7 @@ export default function Dashboard() {
       <div className="album py-5 bg-body-tertiary">
         <div className="container">
           <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {trips.length > 0 ? (
+            {trips && trips.length > 0 ? (
               trips.map((trip, index) => <TripCard key={index} trip={trip} />)
             ) : (
               <div className="text-center">
